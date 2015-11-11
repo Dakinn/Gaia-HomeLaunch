b0e1f74d63781d7d9571add2bbc7769ed6a3aa94
Austin <austin@sharpideas.com>
2015-11-11 09:42:40 -0700

    Cleanup CSS, Font, Add Meta, removed local font files, optimized images

--- js/script.js
+++ js/script.js
@@ -1,43 +1,5 @@
 // JavaScript Document
 $( document ).ready(function() {
-//create array for video slides	
-var VideoTitle = [];
-function disp( titles ) {
-  for ( var i = 0; i < titles.length; i++ ) {
-    VideoTitle.push( titles[ i ].innerHTML );
-  }
-}
-//gather slide titles from here
-disp( $(".video-rotator-slide h3"));
-	//call rotator on video slides
-	$('.video-roator').before('<ul class="video-rotator-nav">').cycle({ 
-		fx:    'fade',
-		speed:  'slow', 
-		timeout: 7000, 
-		pager:  '.video-rotator-nav',
-		next:   '.video-rotator-next', 
-    	prev:   '.video-rotator-prev',
-		pause:   true,
-		pagerAnchorBuilder: paginate
-	});
-	//call rotator on testimonials
-	$('.testimonial-wrapper').cycle({ 
-			fx:    'fade',
-			speed:  'slow', 
-			timeout: 7000, 
-			pause:   true,
-			next:   '.testimonial-next', 
-			prev:   '.testimonial-prev'
-	});
-
-	//populate video rotator pagination based on generated array above
-	function paginate(ind) {
-		if (ind === 0){ return '<li><a href="#">' +  VideoTitle[0]  + '</a></li>';}
-		if (ind === 1){ return '<li><a href="#">' +  VideoTitle[1]  + '</a></li>';}
-		if (ind === 2){ return '<li><a href="#">' +  VideoTitle[2]  + '</a></li>';}
-		if (ind === 3){ return '<li><a href="#">' +  VideoTitle[3] + '</a></li>';}
-		if (ind === 4){ return '<li><a href="#">' +  VideoTitle[4]  + '</a></li>';}
-	}
 	
 	//footer funcitonality
 	$('.footer-parent').click(function(){
@@ -49,7 +11,7 @@ disp( $(".video-rotator-slide h3"));
 		var iframes = $('.vimeo-player'),
 			status = $('.status');
 	
-		$('.video-rotator-nav, .about-vimeo-close').bind('click', function () {
+		$('.about-vimeo-close').bind('click', function () {
 			iframes.each(function() {
 			  var player=$f(this);
 			  player.api("pause");
@@ -72,8 +34,8 @@ disp( $(".video-rotator-slide h3"));
 	});
 	
 	//fadein Large images on load
-	$('.KB-img img').hide().one("load",function(){
-		$(this).fadeIn(500);
+	$('.KB-img img, .hero-img-scroller img').one("load",function(){
+		$(this).addClass('fadeIn');
 		}).each(function(){
 			if(this.complete){ $(this).trigger("load");
 		}
